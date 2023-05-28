f43c232f61a49ae758ed73b15e6d36c2b6f84630
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2023-05-28 15:32:25 +0100

    Make Monday the first day of the week

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	let datepicker = document.querySelectorAll(".datepicker");
 	M.Datepicker.init(datepicker, {
 		format: "dd mmmm, yyyy",
-		i18n: {done: "Select"}
+		i18n: {done: "Select"},
+		firstDay: 1
 	});
 
 	// select initialization
